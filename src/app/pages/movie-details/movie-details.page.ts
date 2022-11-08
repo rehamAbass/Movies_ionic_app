f2091c32927bbc66daae43b29afd44c2bcce2749
reham abass <soft.rehamabass@gmail.com>
2022-11-08 20:59:47 +0200

    movie details ..

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
-
-  constructor() { }
+  imageBaseUrl = environment.images;
+  movie = null;
+  constructor(private route: ActivatedRoute ,
+     private movieService: MovieService) { }
 
   ngOnInit() {
+  const id = this.route.snapshot.paramMap.get('id');
+  console.log('id = '+id);
+    this.movieService.getMovieDetails(id).subscribe((res)=>{
+      console.log('res = ',res);
+      this.movie = res;
+    });
   }
 
 }
